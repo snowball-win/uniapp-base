380ea7b75c890db2d94f4ebe3e2f6c44f4cd4e71
李俊杰 <1091457391@qq.com>
2023-06-13 01:10:36 +0800

    feat: 调试播放器

--- src/components/video-player/video-player.vue
+++ src/components/video-player/video-player.vue
@@ -10,8 +10,24 @@
             @touchstart="touchStart"
             @touchend="touchEnd"
         >
-            <swiper-item v-for="(item, index) in 5" :key="index">
-                <view class="swiper-item">视频{{ index }}</view>
+            <swiper-item class="swiper-item" v-for="(item, index) in videoList" :key="index">
+                <video
+                    id="myVideo"
+                    controls
+                    :class="item.direction"
+                    :src="item.url"
+                    :show-fullscreen-btn="true"
+                    :enable-play-gesture="true"
+                    :enable-progress-gesture="true"
+                    :show-center-play-btn="true"
+                    :show-play-btn="false"
+                    :autoplay="false"
+                    object-fit="cover"
+                    @error="videoErrorCallback"
+                >
+                </video>
+                <view class="operate">点赞</view>
+                <view class="collect">收藏</view>
             </swiper-item>
         </swiper>
     </view>
@@ -21,15 +37,46 @@
 
 <script setup lang="ts">
 import request from '@/utils/request'
-import { defineComponent, ref } from 'vue'
+import { ref, reactive } from 'vue'
 import { useStore } from 'vuex'
+
+interface videoItem {
+    url ?: string;
+    direction ?: string;
+}
+let videoList: videoItem []  = [
+    {
+        url: 'https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20200317.mp4',
+        direction: 'Horizontal' // Horizontal 横向 vertical 竖向
+    },
+    {
+        url: 'https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20200317.mp4',
+        direction: 'vertical'
+    },
+    {
+        url: 'https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20200317.mp4',
+        direction: 'Horizontal'
+    },
+    {
+        url: 'https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20200317.mp4',
+        direction: 'vertical'
+    },
+    {
+        url: 'https://img.cdn.aliyun.dcloud.net.cn/guide/uniapp/%E7%AC%AC1%E8%AE%B2%EF%BC%88uni-app%E4%BA%A7%E5%93%81%E4%BB%8B%E7%BB%8D%EF%BC%89-%20DCloud%E5%AE%98%E6%96%B9%E8%A7%86%E9%A2%91%E6%95%99%E7%A8%8B@20200317.mp4',
+        direction: 'Horizontal'
+    }
+]
+
+const videoErrorCallback = (e: any) => {
+    console.log(e)
+}
 
 const store = useStore()
 const title = ref(store.state.system.title)
 // 自动播放
 const autoplay:boolean = false
 // 滑动动画时长
-const duration:number = 500
+const duration:number = 300
 const changeplay = (res: any) => {
     console.log("34", res)
     if(touchStartPageY < touchEndPageY){
@@ -54,6 +101,10 @@ const apiTest = () => {
         console.log(res)
     })
 }
+uni.setTabBarBadge({ //显示数字  
+	index: 1, //tabbar下标
+	text: '6' //数字
+})
 </script>
 
 <style lang="scss">
@@ -63,6 +114,34 @@ const apiTest = () => {
     background: #ccc;
     .swiper{
         height: 100%;
+        &-item{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            position: relative;
+            .vertical{
+                width: 100%;
+                height: 100%;
+            }
+            .Horizontal{
+                width: 100%;
+                height: 200px;
+            }
+            .operate{
+                position: absolute;
+                bottom: 100px;
+                right: 10px;
+                color: #ff0000;
+                font-size: 18px;
+            }
+            .collect{
+                position: absolute;
+                bottom: 50px;
+                right: 10px;
+                color: #ff0000;
+                font-size: 18px;
+            }
+        }
     }
 }
 </style>
