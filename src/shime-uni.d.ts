4f4c5a99415973a88e256003611a43680e3e8f45
李俊杰 <1091457391@qq.com>
2023-06-20 14:51:54 +0800

    feat: 项目基础配置调试

--- src/shime-uni.d.ts
+++ src/shime-uni.d.ts
@@ -21,10 +21,10 @@ declare module 'vuex' {
 }
 
 // 扩展mock
-// declare module 'mockjs' {
-//     /** 所有已注册的mock规则  */
-//     const _mocked: Record<string, any>
-// }
+declare module 'mockjs' {
+    /** 所有已注册的mock规则  */
+    const _mocked: Record<string, any>
+}
 
 // 这个导出一个东西也可以，或者上面引入vue
 export {}
